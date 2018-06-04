30c23cfac777b3cbf7c55c9eff7c6250078abea4
Mustafa Zorbaz <mustafazorbaz7@gmail.com>
2018-06-05 01:02:31 +0300

    Some Test Cases 1.0
    *test_2_webSiteLoginPage
    *test_3_webSiteLogin

--- src/test/java/com/n11/test/Setting.java
+++ src/test/java/com/n11/test/Setting.java
@@ -6,6 +6,7 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Setting {
@@ -15,10 +16,17 @@ public class Setting {
 	 
 	@BeforeClass
 	public static void before(){
-		System.setProperty("webdriver.gecko.driver", "C:\\driver\\geckodriver.exe"); 
-		driver=new FirefoxDriver();
-		 
-	} 
+		//System.setProperty("webdriver.gecko.driver", "C:\\driver\\chromedriver.exe"); 
+		//driver=new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver.exe"); 
+		driver=new ChromeDriver();
+		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
+	}
+	/*@AfterClass
+	public static void after() throws InterruptedException{
+		Thread.sleep(3000);
+		driver.quit();
+	} */
 	public String getUrl(){
 		return webUrl;
 	}
--- src/test/java/com/n11/test/TestCases.java
+++ src/test/java/com/n11/test/TestCases.java
@@ -1,19 +1,48 @@
 package com.n11.test;
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.google.common.base.Predicate;
 import com.n11.pages.Page;
 
-public class TestCases extends Page { 
-	 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestCases extends Page {
+	WebDriverWait wait= new WebDriverWait(driver, 500);
+	  
 	@Test
-	public void webSiteControl(){ 
+	public void test_1_webSiteControl(){ 
 		driver.get(getUrl());
+		waitForPageLoad(); 
+		Assert.assertTrue(driver.getTitle().equals("n11.com - Alışverişin Uğurlu Adresi"));
 		System.out.println("N11 Alışveriş sitesi başarılı bir şekilde açıldı");
-		 
+	}  
+	@Test
+	public void test_2_webSiteLoginPage(){ 
+		findByClass("btnSignIn").click();
+		wait.until(elementClickableById("loginButton")); 
+		Assert.assertTrue(driver.getTitle().equals("Giriş Yap - n11.com"));
+		System.out.println("Kullanıcı girişi için sayfa başarılı bir şekilde açıldı...");
+	}
+	@Test
+	public void test_3_webSiteLogin(){  
+		findById("email").sendKeys("mustafazorbaz7@gmail.com");
+		findById("password").sendKeys("deneme123456"); 
+		findById("loginButton").click();
+		waitForPageLoad(); 
+		Assert.assertTrue(findByClass("user").getText().equals("Mustafa Zorbaz"));
+		System.out.println("Kullanıcı Girişi Başarılı ...");
+	 
+	}
+	
+	
 
-	} 
+	
 
 }
